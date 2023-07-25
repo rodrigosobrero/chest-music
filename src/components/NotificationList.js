92c48174c865f8632258612ee8be66d9a1a48a67
dylangavilan <dylan.gavilan32@gmail.com>
2023-07-25 20:14:38 -0300

    add accepted, denied

--- src/components/NotificationList.js
+++ src/components/NotificationList.js
@@ -4,10 +4,11 @@ import NotificationRow from './NotificationRow'
 export const NotificationList = ({invites}) => {
   return (
     <>
-      <div className='w-[42.5rem] bg-neutral-silver-700 flex min-h-64 flex-col p-8 space-y-2 rounded-lg'>
+      <div className='w-[42.5rem] bg-neutral-silver-700 flex h-[80%] overflow-y-auto flex-col p-8 space-y-2 rounded-lg'>
           {invites?.map((el) => (
               <NotificationRow invite={el}/> 
           ))}
+          
       </div>
     </>
   )
--- src/components/NotificationRow.js
+++ src/components/NotificationRow.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { formatDate, formatTime } from 'utils/helpers'
 import NotificationButton from './NotificationButton'
+import { CheckIcon } from "@heroicons/react/24/solid";
+
 const NotificationRow = ({invite}) => {
   const [ isOpen, setIsOpen ] = React.useState(false)
     console.log(formatTime(invite.track.date))
@@ -19,11 +21,15 @@ const NotificationRow = ({invite}) => {
           </div>
           <div className='flex space-x-4'>
               <div>
-                  <span className=''>
+                  <span className='text-neutral-silver-200'>
                       {formatDate(invite.track.date)}
                   </span>
               </div>
-              <NotificationButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)}/>
+              {invite.status === 'pending' ? <NotificationButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)}/> : 
+               invite.status === 'accepted' ? 
+               <span className='text-white capitalize flex items-center'>
+                     <CheckIcon className="h-5 w-5 text-white" /> accepted
+               </span> : <span className='text-neutral-silver-200 capitalize'>denied</span> }
           </div>
         </div>
        { isOpen &&   
