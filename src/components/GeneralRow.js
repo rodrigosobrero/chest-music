fbeab5b2e34539c99b9c21d3fa30527c896eecfb
dylangavilan <dylan.gavilan32@gmail.com>
2023-08-01 23:46:06 -0300

    add hover notification

--- src/components/GeneralRow.js
+++ src/components/GeneralRow.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NotificationIcon from './NotificationIcon'
 import { formatDate } from 'utils/helpers'
 const GeneralRow = ({ notification }) => {
+  const [toggle, setToggle] = useState(false)
+  const handleChange = () => setToggle(!toggle)
+  console.log(toggle)
   return (
         <>
-            <div className='row flex justify-between'>
+            <div className={`row flex justify-between ${toggle && 'over'}`} onMouseEnter={handleChange} onMouseLeave={handleChange}>
             <div className='flex space-x-4 items-center'>
-               <NotificationIcon  type={notification.data.status} iconStyle='h-5 w-5 text-white' 
+               <NotificationIcon  type={notification.data.status} iconStyle={`h-5 w-5 ${toggle ? 'text-brand-gold' : 'text-white'}`} 
                 containerStyle={'bg-neutral-black rounded-lg flex justify-center items-center h-10 w-10'}/>
                 <div>
                     <div className='text-base'>{notification.data.title}</div>
--- src/components/NotificationIcon.js
+++ src/components/NotificationIcon.js
@@ -2,19 +2,13 @@ import React from 'react'
 import { MusicalNoteIcon, XMarkIcon, LinkIcon, CheckCircleIcon } from "@heroicons/react/24/solid";
 
 const NotificationIcon = ({ type, iconStyle, containerStyle }) => {
-  console.log(type)
   const notificationIcons = {
     invite_accepted: LinkIcon,
     invite_denied: XMarkIcon,
     track_uploaded: CheckCircleIcon,
     track_shared: MusicalNoteIcon
-    // Agrega más tipos de notificaciones y sus respectivos SVGs aquí
   };
-
-  // Determinar el nombre del archivo SVG según el tipo de notificación
   const IconComponent = notificationIcons[type] || MusicalNoteIcon;
-
-
   return (
     <>
        <div className={containerStyle}>
