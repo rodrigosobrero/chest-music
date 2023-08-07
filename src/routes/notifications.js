3913c791bb4bec28aebcfd7cd7343e607515eedc
Dylan <dylan.gavilan32@gmail.com>
2023-08-07 11:40:02 -0300

    add redirect

--- src/routes/notifications.js
+++ src/routes/notifications.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import notifications from 'data/notifications.json'
 import Notification from 'components/Notification'
+import { Cog8ToothIcon } from "@heroicons/react/24/solid";
+import { NavLink } from 'react-router-dom';
 export default function Notifications() {
   return (
     <>
       <div className='notification'>
-        <div>
+        <div className='space-y-6'>
           <h3>
             notifications
           </h3>
-          <p>
-            manage
-          </p>
+          <NavLink to={'manage'}>
+            <p className='text-brand-gold flex justify-center'>
+              <Cog8ToothIcon className=' h-5 w-5' />
+              Manage
+            </p>
+          </NavLink>
           <Notification notifications={notifications} />
         </div>
       </div>
