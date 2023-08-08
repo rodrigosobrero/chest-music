2d2bbb75750ce8d490c8845368c58ed9b2b5985c
dylangavilan <dylan.gavilan32@gmail.com>
2023-08-08 16:02:51 -0300

    add helper time ago

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -17,5 +17,39 @@ function formatDate(date) {
 function formatTime(seconds) {
   return (seconds / 60).toFixed(2).toString().replace('.', ':');
 }
+function timeDifference( previous) {
+  var current = new Date()
 
-export { classNames, formatDate, formatTime }
+  var msPerMinute = 60 * 1000;
+  var msPerHour = msPerMinute * 60;
+  var msPerDay = msPerHour * 24;
+  var msPerMonth = msPerDay * 30;
+  var msPerYear = msPerDay * 365;
+
+  var elapsed = current - previous;
+
+  if (elapsed < msPerMinute) {
+       return Math.round(elapsed/1000) + ' seconds ago';   
+  }
+
+  else if (elapsed < msPerHour) {
+       return Math.round(elapsed/msPerMinute) + ' minutes ago';   
+  }
+
+  else if (elapsed < msPerDay ) {
+       return Math.round(elapsed/msPerHour ) + ' hours ago';   
+  }
+
+  else if (elapsed < msPerMonth) {
+      return Math.round(elapsed/msPerDay) + ' days ago';   
+  }
+
+  else if (elapsed < msPerYear) {
+      return Math.round(elapsed/msPerMonth) + ' months ago';   
+  }
+
+  else {
+      return Math.round(elapsed/msPerYear ) + ' years ago';   
+  }
+}
+export { classNames, formatDate, formatTime, timeDifference }
