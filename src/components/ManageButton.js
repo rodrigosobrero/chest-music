b981820ce15fb0ee15a0ad27cd43bd601bbaf405
Dylan <dylan.gavilan32@gmail.com>
2023-08-15 15:21:24 -0300

    nuevos modales

---
+++ src/components/ManageButton.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import Modal from './Modal'
+
+const ManageButton = ({ isOpen }) => {
+  const [show, setShow] = useState(false)
+  const toggle = () => setShow(!show);
+  const textBlock = {
+    title: 'BLOCK NEW USER',
+    subtitle: '<span>Search the desired user below to stop receiving their invites</span>',
+    button: 'Block'
+  }
+  const textAllow = {
+    title: 'ALLOW NEW USER',
+    subtitle: '<span>Search the desired user below to allow their invites</span>',
+    button: 'Allow'
+  }
+  const ModalContent = () => (
+    <div>
+        <div className='text-center'>
+            <div className='px-4 gap-y-3'>
+                <h3>{isOpen ? textBlock.title : textAllow.title}</h3>
+                <div className='text-lg text-neutral-silver-200' 
+                    dangerouslySetInnerHTML={{__html: isOpen ? textBlock.subtitle : textAllow.subtitle}}/>
+            </div>
+            <div className='text-left mt-3'>
+                <label>User</label>
+                <input type='search' placeholder='Search..' 
+                className='border w-full rounded-xl border-neutral-silver-400 bg-neutral-silver-700 p-4 outline-none'/>
+            </div>
+            <div className='gap-4 flex self-stretch	mt-10'>
+                <button onClick={toggle} className='px-6 py-3 w-full bg-neutral-silver-600 rounded-[10px]'>Cancel</button>
+                <button onClick={toggle} className='px-6 py-3 w-full bg-neutral-silver-500 rounded-[10px]'>
+                    {isOpen ? textBlock.button : textAllow.button}
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+  return (
+    <>
+       <p className='text-brand-gold font-archivo text-base cursor-pointer' onClick={toggle}>+  Add another</p>
+       <Modal show={show}>
+           <ModalContent></ModalContent>
+       </Modal>
+    </>
+  )
+}
+
+export default ManageButton
--- src/routes/manage.js
+++ src/routes/manage.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
+import ManageList from 'components/ManageList';
+import Modal from 'components/Modal';
+import ManageButton from 'components/ManageButton';
 import unlocked from 'assets/images/icon-unlocked.svg';
 import locked from 'assets/images/icon-lock.svg'
-import ManageList from 'components/ManageList';
 import manage from 'data/manage.json'
-import Modal from 'components/Modal';
 export default function Manage() {
   const [isOpen, setIsOpen] = useState(false)
   const [show, setShow] = useState()
   const toggle = () => setShow(!show)
-  
+
   return (
     <>
       <Modal show={show}>
@@ -30,7 +31,7 @@ export default function Manage() {
         </div>
       </Modal>
       <div className='flex justify-center flex-col space-y-2 items-center h-full px-20'>
-        <div className='bg-neutral-black h-24 w-full rounded-t-3xl rounded-b-md flex items-center justify-between px-20 '>
+        <div className='bg-neutral-black h-28 w-full rounded-t-3xl rounded-b-md flex items-center justify-between px-20'>
             <h3 className='uppercase'>{!isOpen ? 'BLOCKED USERS' : 'ALLOWED USERS' }</h3>
             <div className='p-2 flex items-center bg-neutral-silver-600 rounded-xl space-x-4'>
               <div className='bg-neutral-black rounded-lg flex justify-center items-center h-10 w-10'>
@@ -41,14 +42,15 @@ export default function Manage() {
                   <span className='text-xs text-neutral-silver-200 font-normal'>
                     {isOpen ? 'Receive notifications from anyone except blocked' : 'Receive notifications from allowed users only'}
                   </span>
-               </div>
-               <button className='bg-neutral-silver-700 text-brand-gold capitalize px-2 text-sm rounded-xl' onClick={toggle}>
-                  change
-               </button>
+              </div>
+              <button className='bg-neutral-silver-700 text-brand-gold capitalize px-2 text-sm rounded-xl' onClick={toggle}>
+                  change                  
+              </button>
             </div>
         </div>
-        <div className='bg-neutral-black h-3/5 w-full rounded-b-3xl rounded-t-md px-20'>
+        <div className='bg-neutral-black w-full rounded-b-3xl rounded-t-md py-10 px-20'>
              <ManageList data={manage} privacyIsOpen={isOpen}/> 
+             <ManageButton isOpen={isOpen} />
         </div>
       </div>
     </>
