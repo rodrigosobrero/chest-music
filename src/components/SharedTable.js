552820b65a28351bd1a1f30d1d3aa2e2153711e1
dylangavilan <dylan.gavilan32@gmail.com>
2023-08-18 11:31:30 -0300

    cambio json, css tablas

--- src/components/SharedTable.js
+++ src/components/SharedTable.js
@@ -1,21 +1,31 @@
 import React from 'react'
-
+import empty from 'assets/images/empty-chest.svg';
 import SharedList from './SharedList'
 const SharedTable = ({artist, data}) => {
   console.log(data)
   return (
     <>
-        <div className='bg-neutral-black pb-6 pt-8 w-full rounded-t-3xl rounded-b-md flex items-center justify-between px-[60px] '>
-          <div className='flex items-center gap-4'>
+      <div className='flex flex-col gap-1'>
+       <div className='bg-neutral-black pb-6 pt-8 w-full rounded-t-3xl rounded-b-md flex items-center justify-between px-[60px] '>
+         {artist && <div className='flex items-center gap-4'>
             <h4 className='font-thunder uppercase text-[48px] font-bold'>{artist}</h4>
             <div className='py-1 px-3 bg-neutral-silver-700 rounded-lg text-neutral-silver-200 font-archivo'>
               {data?.length} tracks
             </div>
-          </div>
+          </div>}
         </div>
         <div className='bg-neutral-black rounded-b-[32px] rounded-t-xl px-[60px] pt-10 pb-[60px]'>
-              <SharedList tracks={data} /> 
+             {data.length > 0 ?  <SharedList tracks={data} /> :               
+                      <div className='flex flex-col items-center gap-2'>
+                         <h4>Nothing here</h4>
+                         <p className='text-lg text-neutral-silver-200 font-light mb-10'>
+                             Nobody shared a track with you yet
+                         </p>
+                         <img src={empty} alt='' width={240} height={128} className='mb-5' />
+                      </div>
+              }
         </div>
+      </div>
      </>
   )
 }
--- src/routes/shared.js
+++ src/routes/shared.js
@@ -1,19 +1,22 @@
+import SearchBar from "components/SearchBar"
 import SharedTable from "components/SharedTable"
 import shared from "data/shared.json"
 export default function Shared() {
   return (
     <>
-       <div className='text-center font-archivo p-[3.75rem]'>
-          <div>
+       <div className='text-center font-archivo p-[3.75rem] gap-y-4'>
+          <div className='flex flex-col items-center gap-4 mb-10'>
               <h3>Shared with me</h3>
-              <span className="text-neutral-silver-200 text-lg">Explore and play the tracks that other artists shared with you</span>
+              <span className='text-neutral-silver-200 text-lg'>Explore and play the tracks that other artists shared with you</span>
+              <SearchBar onChange={() => console.log('onChange')}/>
           </div>
+          <div className='space-y-6'>
           {
             shared.map((el) => (
               <SharedTable artist={el.username} data={el.tracks}/>
             ))
           }
- 
+          </div>
       </div>
     </>
   )
