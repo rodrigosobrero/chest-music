a53802ac3d262ea58f02e2aac185d9a00ee2945a
RodrigoSobrero <hola@rodrigosobrero.com>
2023-08-22 15:16:42 -0300

    Sign up mobile

--- src/components/Input.js
+++ src/components/Input.js
@@ -28,9 +28,9 @@ export default function Input({ type, placeholder, label, name, value, onChange,
             name={name}
             value={value}
             onChange={onChange}
-            className='border border-neutral-silver-400 bg-neutral-silver-700 rounded-xl p-4 w-full focus:outline-none focus:border-brand-gold' />
+            className='border border-neutral-silver-400 bg-neutral-silver-700 rounded-xl p-4 w-full focus:outline-none focus:border-brand-gold leading-5' />
           {showHide &&
-            <div className='absolute top-5 right-5'>
+            <div className='absolute top-4 right-4'>
               <button type='button' onClick={showHidePassword}>
                 {inputType === 'password' ?
                   <EyeSlashIcon className='h-5 w-5 text-neutral-silver-500' /> :
