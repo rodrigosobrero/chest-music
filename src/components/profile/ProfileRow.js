b81db713850e7169262f21baeee4a9ffa9a48c2d
dylangavilan <dylan.gavilan32@gmail.com>
2023-09-13 14:55:05 -0300

    cambios css row profile

--- src/components/profile/ProfileRow.js
+++ src/components/profile/ProfileRow.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import { MusicalNoteIcon, XMarkIcon, LinkIcon, CheckCircleIcon } from "@heroicons/react/24/solid";
+import { ChevronRightIcon } from "@heroicons/react/24/solid";
+
+
 const ProfileRow = ({ title, subtitle }) => {
   return (
     <>
-       <div className='font-archivo  mx-auto bg-neutral-black p-4 flex items-center space-x-4 rounded-xl'>  
-            <div className={'bg-neutral-silver-700 rounded-lg flex justify-center items-center h-10 w-10'}>
-                <MusicalNoteIcon  className='h-5 w-5 text-brand-gold' />
+       <div className='font-archivo mx-auto bg-neutral-black p-4  rounded-xl flex items-center justify-between'>  
+            <div className='flex items-center space-x-4'>
+                <div className={'bg-neutral-silver-700 rounded-xl flex justify-center items-center p-4'}>
+                    <MusicalNoteIcon  className='h-7 w-7 text-brand-gold' />
+                </div>
+                <div>
+                    <h4 className='text-lg font-archivo font-semibold'>{title}</h4>
+                    <h4 className='text-base text-neutral-silver-300'>{subtitle}</h4>
+                </div>
             </div>
             <div>
-                <h4 className='text-lg font-archivo font-semibold'>{title}</h4>
-                <h4 className='text-base text-neutral-silver-300'>{subtitle}</h4>
+                <ChevronRightIcon className='h-6 w-6 text-silver-gray-200' />
             </div>
        </div>
     </>
