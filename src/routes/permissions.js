3c848380fe46b0554c767f2db315b8e75bcd4197
dylangavilan <dylan.gavilan32@gmail.com>
2023-09-15 14:33:37 -0300

    permissions

--- src/routes/permissions.js
+++ src/routes/permissions.js
@@ -3,20 +3,29 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 import permissions from 'data/permissions.json'
 import PermissionsList from 'components/profile/PermissionsList'
+import icon from 'assets/images/icon-exclamation-circle.svg'
+import ManageButton from 'components/notifications/ManageButton'
 const Permissions = () => {
   const location = useLocation()
   return (
     <>
       <div className='px-[60px]'>
         <Ubication path={location.pathname} />
-        <div className='flex flex-col mt-5 mb-8'>
+        <div className='flex justify-between mt-5 mb-8'>
                 <div>
                     <h3 className='font-thunder-bold text-5xl font-bold'>Global permissions</h3>
                     <h5 className='text-neutral-silver-200 text-lg'>Choose users to be your regular listeners by giving them the key to your chest</h5>
                 </div>
+                <div className='p-3 bg-neutral-black w-[348px] rounded-xl flex items-center gap-3'> 
+                  <img src={icon} className='h-6 w-6'/>
+                  <span className='text-neutral-silver-300 text-sm'>
+                  Keep in mind that these users will be able to listen to all your tracks without limit by default
+                  </span>
+                </div>
         </div>
         <div className='bg-neutral-black w-full rounded-3xl px-6 py-4 xl:py-10 xl:px-20'>
                 <PermissionsList data={permissions} /> 
+                <ManageButton />
         </div>
       </div>
     </>
