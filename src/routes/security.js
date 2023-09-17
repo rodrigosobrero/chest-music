edddbf98b480ccbe1db872ab1a0089dab0517610
dylangavilan <dylan.gavilan32@gmail.com>
2023-09-17 15:04:22 -0300

    security

--- src/routes/security.js
+++ src/routes/security.js
@@ -1,7 +1,34 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom';
 import Ubication from 'components/Ubication';
+import { KeyIcon } from "@heroicons/react/24/solid";
+import pencil from 'assets/images/icon-pencil-alt.svg'
+import { ReactComponent as ViewGrid } from 'assets/images/icon-view-grid.svg'
+import { ReactComponent as Elipse } from 'assets/images/icon-elipse.svg'
 const Security = () => {
+    const Casillero = ({ title, icon, quantity }) => {
+        const ellipses = new Array(quantity).fill(null).map((_, index) => (
+            <Elipse key={index} /> 
+          ));
+        return  (
+            <div className='w-2/4 p-8 bg-neutral-black rounded-2xl flex justify-between font-archivo items-center '>
+                <div className='flex font-semibold gap-6 items-center'>
+                    <div>
+                       {icon}
+                    </div>
+                    <div className='flex flex-col text-[22px] '> 
+                       {title}
+                       <div className='flex gap-x-1.5'>
+                         {ellipses}
+                       </div>
+                    </div>
+                </div>
+                <div className='text-brand-gold text-lg font-semibold flex items-center gap-x-1.5 cursor-pointer'>
+                    <img src={pencil} className='h-6 w-6'/>
+                    Edit
+                </div>
+            </div>
+    )}
     const location = useLocation();
     return (
       <>
@@ -15,23 +42,19 @@ const Security = () => {
          </div>
          <div className='w-full flex flex-col gap-y-6'>
             <div className='w-full flex space-x-6'>
-                <div className='w-2/4 p-8 bg-neutral-black rounded-2xl'>
-                    <div>
-                        pin code
-                    </div>
-                    <div>edit</div>
-                </div>
-                <div className='w-2/4 p-8 bg-neutral-black rounded-2xl'>
-                </div>
+              <Casillero title={'PIN Code'} icon={<ViewGrid className="h-8 w-8 " />} quantity={4}/>
+              <Casillero title={'Password'} icon={<KeyIcon className="h-8 w-8 text-gray-500" />} quantity={8}/>
             </div>
             <div className='w-full bg-neutral-black p-8 rounded-3xl'>
                 <h4 className='font-archivo text-[22px]'>Chest keeps your treasure secure</h4>
                 <p className='text-neutral-silver-200 text-left'>
                     Aliquet malesuada dolor sit amet consectetur. Viverra id feugiat risus volutpat dictum. Fames adipiscing nunc pretium dolor at enim eu.
                 </p>
-                <a className='font-archivo text-xl text-brand-gold' href='!#'>
-                    Learn more
-                </a>
+                <p className='text-left mt-4'>
+                    <a className='font-archivo text-xl text-brand-gold ' href='!#'>
+                        Learn more
+                    </a>
+                </p>
             </div>
          </div>
         </div>
