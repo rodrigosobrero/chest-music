8a0ede9e7e4dbc5880f3d62843f16353862b55dd
dylangavilan <dylan.gavilan32@gmail.com>
2023-11-07 10:51:14 -0300

    cambios

--- src/routes/shared.js
+++ src/routes/shared.js
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import SearchBar from "components/SearchBar"
 import SharedTable from "components/shared/SharedTable"
 import { useTranslation } from "react-i18next"
 import { useFetch } from "hooks/useFetch"
 import { useDispatch, useSelector } from "react-redux"
 import { apiUrl } from "utils/api"
+import Loading from "components/Loading"
 export default function Shared() {
   const { t } = useTranslation()
   const user = useSelector((state) => state.auth.user )
@@ -27,7 +28,7 @@ export default function Shared() {
   return (
     <>     
        <div className='flex flex-col  md:container px-3 py-10 md:p-[60px] gap-y-6 md:gap-y-10 text-center font-archivo '>
-          <div className='flex flex-col items-center gap-y-2 px-3 md:px-0'>
+          <div className='flex flex-col items-center gap-y-2 px-3 md:px-0 '>
               <h3 className="text-[64px] leading-[44px] md:leading-[58px]">
                  {t('shared.title')}
               </h3>
@@ -38,8 +39,8 @@ export default function Shared() {
                <SearchBar className='!border-[1.5px] placeholder:text-center focus:border-brand-gold' onChange={handleChange}/>
               </div>
           </div>
-          <div className='flex flex-col gap-y-1'>
-          {isFetching ? <h3>Loading</h3>  : data.length > 0 ? 
+          <div className='flex flex-col gap-y-1 items-center text-center'>
+          {isFetching ? <Loading />  : data.length > 0 ? 
             filteredTracks?.map((el) => (
               <SharedTable artist={el.artist} data={el.tracks} dispatch={dispatch}/>
             )) 
