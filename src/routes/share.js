d1ca43abd9d4a710fdf5e1c2c17a1fa905c1eac8
dylangavilan <dylan.gavilan32@gmail.com>
2023-12-04 14:22:19 -0300

    #110

--- src/routes/share.js
+++ src/routes/share.js
@@ -9,10 +9,15 @@ import OptionSectionDesktop from 'components/share/OptionSectionDesktop';
 import Reel from 'components/share/sections/ReelTiktok';
 import axios from 'axios';
 import { apiUrl } from 'utils/api';
-import { useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 const Share = () => {
+  const navigate = useNavigate()
+  const goBack = () => {
+    navigate(-1)
+  }
   const [ status, setStatus ] = React.useState('generate');
+  const location = useLocation()
   const [ isUnlimited, setIsUnlimited ] = React.useState(false)
   const user = useSelector((state) => state.auth.user);
   const [track, setTrack] = useState({})
@@ -27,14 +32,21 @@ const Share = () => {
     { headers: { Authorization: `Bearer ${user?.token}` } })
     .then(({data}) => setTrack(data))
   }, [user?.token, trackId])
+  useEffect(() => {
+    if(location?.search) {
+      let method = location.search.split('?=')
+      method = method[method.length - 1] === 'sendDM' && 'send' 
+      setStatus(method)
+    }
+  }, [location])
   return (
     <>
       <div className='md:w-[1200px] md:p-[60px]  md:rounded-3xl  w-full  pt-8 pb-10  bg-neutral-black md:my-10 md:gap-y-8'>
         <div className='flex flex-col items-center md:gap-y-6 gap-y-4'>
            <h3 className='md:text-[64px] text-[48px] text-center'> SHARE YOUR TREASURE </h3>
-           <img src={track.cover} alt='track cover' className='md:w-[200px] md:h-[200px] w-[140px] h-[140px]'/>
+           <img src={track.cover_url} alt='track cover' className='md:w-[200px] md:h-[200px] w-[140px] h-[140px]'/>
            <div className='text-center flex flex-col md:gap-y-2 gap-y-1.5'>
-             <h4 className='md:text-[22px] text-base uppercase font-semibold'>{track?.name}</h4>
+             <h4 className='md:text-[22px] leading-[26px] text-base uppercase font-semibold'>{track?.name}</h4>
              <h6 className='md:text-xl text-base text-neutral-silver-200'>
                {track?.versions?.length > 0 && track?.versions[0].name}
              </h6>
@@ -42,11 +54,11 @@ const Share = () => {
           <div className='flex flex-col w-full md:items-center md:mt-0 mt-4'>
           <OptionSectionDesktop status={status} changeStatus={changeStatus} />
           <OptionSectionMobile status={status} changeStatus={changeStatus} />
-              {status === 'generate' && <LinkGenerate versionId={track?.versions?.length > 0 && track?.versions[0].id} token={user?.token}/>}
-              {status === 'post' && <PostTwitter toggleUnlimited={toggleUnlimited}/>}
-              {status === 'story' && <Story token={user?.token} />}
-              {status === 'send' && <SendDM toggleUnlimited={toggleUnlimited} token={user?.token} versionId={track?.versions?.length > 0 && track?.versions[0].id}/>}
-              {status === 'reel' && <Reel />}
+              {status === 'generate' && <LinkGenerate versionId={track?.versions?.length > 0 && track?.versions[0].id} token={user?.token} onCancel={goBack}/>}
+              {status === 'post' && <PostTwitter toggleUnlimited={toggleUnlimited} onCancel={goBack}/>}
+              {status === 'story' && <Story token={user?.token} onCancel={goBack}/>}
+              {status === 'send' && <SendDM onCancel={goBack} toggleUnlimited={toggleUnlimited} token={user?.token} versionId={track?.versions?.length > 0 && track?.versions[0].id}/>}
+              {status === 'reel' && <Reel onCancel={goBack} />}
           </div>
         </div>
       </div>
