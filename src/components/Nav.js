100528b030d54a850153ee945c5d1efb1363f487
Dylan <dylan.gavilan32@gmail.com>
2023-12-26 14:03:45 -0300

    #187

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -68,7 +68,7 @@ export default function Nav() {
                   </button>
                 </li>
               )}
-              {location.pathname !== '/setup' &&
+              {location.pathname !== '/setup' && location.pathname !== '/sign-in' &&
                 <li className='flex items-center'>
                   <NavLink to='/notifications' className='p-1'>
                     <BellIcon className='h-6 w-6' />
