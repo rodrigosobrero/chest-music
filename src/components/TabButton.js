f93da620863b5ebc9b7c1387b68cb194d2c6bdfa
dylangavilan <dylan.gavilan32@gmail.com>
2023-12-28 00:39:51 -0300

    tab button

--- src/components/TabButton.js
+++ src/components/TabButton.js
@@ -4,16 +4,16 @@ import TabIcon from './share/TabIcon'
 const TabButton = ({ isActive, icon, text, className, onClick}) => {
   const [isHovered, setIsHovered] = useState(false)
   const toggle = () => setIsHovered(!isActive)
-  console.log(icon)
+  
   return (
     <>
     <div className='flex flex-col'>
-        <button onClick={onClick} onMouseEnter={toggle} onMouseLeave={toggle}
+        <button onClick={onClick} onMouseOvers={toggle} onMouseLeave={toggle}
         className={`py-3 px-4 font-archivo font-semibold text-base  rounded-xl flex items-center gap-x-2 
                   bg-neutral-silver-700 text-neutral-silver-200 hover:text-brand-gold hover:border-brand-gold
                     ${isActive && '!bg-brand-gold !text-neutral-black'}
                     transition-colors duration-600 ${className}` }>
-            {icon && <TabIcon type={icon} status={(isHovered || isActive) ? 'actived' : 'disabled' }/>}
+            {icon && <TabIcon type={icon} s={(isHovered || isActive) ? 'actived' : 'disabled' }/>}
             {text}
         </button>
         <div className={`w-[80px]  mx-auto h-0.5 mt-1.5 border border-brand-gold ${!isActive && 'hidden'}`}></div>
