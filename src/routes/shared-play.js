643af9f7c56c6f5a700a45d2defd55fd4d2b9412
dylangavilan <dylan.gavilan32@gmail.com>
2023-12-28 23:43:25 -0300

    mobile player

--- src/routes/shared-play.js
+++ src/routes/shared-play.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import cover_track from 'assets/images/cover-track.png'
+import rectangle from 'assets/images/icon-rectangle.png'
+
 import { apiUrl } from 'utils/api';
 import Button from 'components/Button';
 import { useDispatch } from 'react-redux';
@@ -9,13 +11,9 @@ import { playing } from 'app/playlist';
 
 const SharedPlay = () => {
   const dispatch = useDispatch()
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const [track, setTrack] = useState({})
   const navigate = useNavigate()
-
-  const playTrack = () => {
-
-  };
   
   useEffect(() => {
     const token = searchParams.get('token')
@@ -33,28 +31,32 @@ const SharedPlay = () => {
       }));
     })
   }, [searchParams])
-
+  console.log(track)
   return (
     <>
-     <div className=' lg:container'>
+     <div className='lg:block hidden lg:container'>
         <div className='lg:pt-[60px] lg:pb-[40px] flex lg:gap-x-12 items-center'>
             <div>
-               <img src={track?.cover_url} className='lg:w-[220px] lg:h-[220px] lg:rounded-lg'/>
+               <img src={track?.cover_url} className='lg:w-[220px] lg:h-[220px] lg:rounded-lg' alt='cover track' />
             </div>
             <div className='flex flex-col '>
                <div className='lg:mb-6'>
-                    <p className='text-left text-neutral-silver-200 text-base'>{track?.album} -- {track?.version_name}</p>
+                    <p className='text-left text-neutral-silver-200 !text-base flex items-center gap-2'>
+                     {track?.album} 
+                     <img src={rectangle} alt='rectangle' className='h-[3px]'/> 
+                     {track?.version_name}
+                    </p>
                </div>
                <div className='lg:mb-3'>
-                    <h2 className='lg:text-[76px] leading-[68px] !font-thunder-bold'>{track?.title}</h2>
+                    <h2 className='lg:!text-[76px] !leading-[68px] !font-thunder-bold'>{track?.title}</h2>
                </div>
                <div>
-                    <p className='text-left lg:text-[22px] capitalize'>{track?.authors?.join(', ')}</p>
+                    <p className='text-left lg:!text-[22px] capitalize'>{track?.authors?.join(', ')}</p>
                </div>
             </div>
         </div>
         <div className='lg:pt-[20px] lg:pb-[80px]'>
-            <div className='lg:p-10 bg-neutral-black flex lg:gap-x-20 '>
+            <div className='lg:p-10 bg-neutral-black flex lg:gap-x-20 rounded-2xl'>
               <div className='w-2/4 h-full flex flex-col gap-y-6'>
                 <h3 className='!text-[64px] !font-thunder-bold pr-10 leading-[58px] text-neutral-silver-200'>
                    If your music is your <br />treasure, it deserves  <br/> to have its chest.
@@ -64,10 +66,31 @@ const SharedPlay = () => {
                 </div>
               </div>
               <div className='w-2/4'>
-                <img src={cover_track} className='w-[620px] h-[300px] rounded-lg' />
+                <img src={cover_track} alt='cover' className='w-[620px] h-[300px] rounded-lg' />
               </div>
             </div>
         </div>
+     </div>
+     <div className='p-3  h-full bg-neutral-black lg:hidden'>
+        <div className='w-full h-[510px] bg-neutral-silver-700 rounded-3xl py-2'>
+          <div className='mb-5 py-2'>
+            <p className='capitalize !text-lg font-semibold'>
+              {track?.album}
+            </p>
+          </div>
+          <div className='flex justify-center'>
+            <img src={track?.cover_url} alt='cover' className='w-[286px] h-[286px] rounded-md'/>
+          </div>
+          <div className='h-[160px] rounded-[20px] px-5 py-2 w-full bg-custom-opacity flex flex-col justify-center items-center'>
+              <h5 className='uppercase font-thunder-bold !text-[36px] !leading-[32px]'>
+                {track?.title}
+              </h5>
+              <p className='!text-base text-neutral-silver-200 capitalize'>
+                 {track?.authors?.join(', ')}
+               </p>
+          </div>
+        </div>
+
      </div>
     </>
   )
