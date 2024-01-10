9c1a1ea5ace3ccdda30b863be6f6ec2afdc0b7f7
Dylan <dylan.gavilan32@gmail.com>
2024-01-10 16:42:49 -0300

    .

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import { api as service } from 'utils/axios';
 import { auth } from 'utils/firebase';
 import { saveUser, updateUserData } from 'app/auth';
 // import { getToken, onMessage } from 'firebase/messaging';
+
 import { onAuthStateChanged, getIdToken, onIdTokenChanged } from 'firebase/auth';
 import { api, useGetAccountQuery } from 'store/api';
 import { store } from 'app/store';
 import { persistStore } from 'redux-persist';
-
 // import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -210,16 +210,16 @@ function App() {
     // });
   }, []);
 
-  // useEffect(() => {
-
-  //    onIdTokenChanged((user) => {
-  //      if (user) {
-  //        user.getIdToken().then((newToken) => {
-  //         dispatch(updateUserData({token: newToken}))
-  //     });}
-  //   });
-    
-  // },[])
+  useEffect(() => {
+    const unsubscribe = onIdTokenChanged(user => {
+      if(user){
+          user.getIdToken().then(newToken => {
+          dispatch(updateUserData({ token: newToken }));
+        });
+      }
+    });
+    return () => unsubscribe();
+  }, []);
 
   return (
     <RouterProvider router={router} />
