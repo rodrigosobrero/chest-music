b9427689d3505bf4a6bd5fafb7395151897d10b4
RodrigoSobrero <hola@rodrigosobrero.com>
2024-01-12 19:46:27 -0300

    catch data.type error

--- src/utils/DisconnectedRoute.js
+++ src/utils/DisconnectedRoute.js
@@ -2,10 +2,10 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 
 export default function DisconnectedRoute({ children }) {
-  const { data, token } = useSelector((state) => state.auth.user);
+  const user = useSelector((state) => state.auth.user);
 
-  return token ? (
-    <Navigate to={data?.type ? '/my-chest' : '/setup' } replace /> 
+  return user?.token ? (
+    <Navigate to={user?.data?.type ? '/my-chest' : '/setup' } replace /> 
   ) : (
     children
   );
