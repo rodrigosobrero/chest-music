e89b127104e242b11b963883d5e2a8e4c6cbc66e
Dylan <dylan.gavilan32@gmail.com>
2024-02-15 16:32:48 -0300

    cweb-80

--- src/components/notifications/GeneralRow.js
+++ src/components/notifications/GeneralRow.js
@@ -1,17 +1,63 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import NotificationIcon from './NotificationIcon'
 import { timeDifference } from 'utils/helpers'
 import { useTranslation } from 'react-i18next'
+import { useDispatch, useSelector } from 'react-redux'
+import { api } from 'utils/axios'
+import { playing, play as togglePlay } from 'app/playlist';
 const GeneralRow = ({ notification }) => {
   const [toggle, setToggle] = useState(false)
   const handleChange = () => setToggle(!toggle)
+  const user = useSelector((state) => state.auth.user);
+  const { playlist } = useSelector((state) => state.playlist);
+  const [play, setPlay] = useState(false);
+
   const { i18n } = useTranslation()
   
+  const dispatch = useDispatch()
+
+  const playTrack = () => {
+    if(notification.type === 'version_shared'){
+        api.get(`/project/version/${notification.version_id}/url`,{
+            headers: { Authorization: `Bearer ${user?.token}` },
+        })
+        .then(({ data }) => {
+            dispatch(playing({
+                id: notification.version_id,
+                album: data.album,
+                cover: data.cover_url,
+                name: data.title,
+                authors: data.authors,
+                type: 'version',
+                audio: data.url,
+                isPlaying: true,
+              }));
+        })
+    }
+  }
+  
+  const handleOnCoverClick = (e) => {
+
+    if (play) {
+      return dispatch(togglePlay())
+    }
+
+    playTrack();
+  }
+
+  useEffect(() => {
+    if(notification.type === 'version_shared') {
+        setPlay(playlist[0]?.id === notification.version_id && playlist[0]?.type === 'version');
+    }
+  }, [playlist, notification.type]);
+
   return (
         <>
-           <div className={`row md:!pr-5 flex  justify-between ${toggle && 'over'} `} onMouseEnter={handleChange} onMouseLeave={handleChange}>
+           <div className={`row md:!pr-5 flex  justify-between ${toggle && 'over'} `} onMouseEnter={handleChange} onMouseLeave={handleChange} onClick={handleOnCoverClick}>
                 <div className='flex space-x-4 items-center'>
-                    <NotificationIcon  type={notification.status} iconStyle={`md:h-7 md:w-7 h-5 w-5 ${toggle ? 'text-brand-gold' : 'text-white'}`} 
+                    <NotificationIcon  type={notification.type === 'version_shared' && play ? 'player' : notification.type} 
+                    iconStyle={`md:h-7 md:w-7 h-5 w-5 ${toggle ? 'text-brand-gold' : 'text-white'}`} 
+                    isPlaying={playlist[0]?.isPlaying}
                     containerStyle={'bg-neutral-black rounded-lg flex justify-center items-center p-2.5 md:p-3'}/>
                     <div>
                         <div className='md:text-xl text-base'>{notification.content[i18n.language === 'en' ? 'en' : 'es'].title}</div>
--- src/components/notifications/NotificationIcon.js
+++ src/components/notifications/NotificationIcon.js
@@ -1,14 +1,21 @@
 import React from 'react'
-import { MusicalNoteIcon, XMarkIcon, LinkIcon, CheckCircleIcon } from "@heroicons/react/24/solid";
+import { MusicalNoteIcon, XMarkIcon, LinkIcon, CheckCircleIcon, PlayIcon, PauseIcon  } from "@heroicons/react/24/solid";
 
-const NotificationIcon = ({ type, iconStyle, containerStyle }) => {
+const NotificationIcon = ({ type, iconStyle, containerStyle, isPlaying }) => {
+  
+  console.log('type', type)
+
   const notificationIcons = {
     invite_accepted: LinkIcon,
     invite_denied: XMarkIcon,
     track_uploaded: CheckCircleIcon,
-    version_shared: MusicalNoteIcon
+    version_shared: MusicalNoteIcon,
+    player: {
+      play: PlayIcon,
+      pause: PauseIcon
+    }
   };
-  const IconComponent = notificationIcons[type] || MusicalNoteIcon;
+  const IconComponent = type === 'player' ? notificationIcons.player[isPlaying ? 'pause' : 'play'] : notificationIcons[type] || MusicalNoteIcon;
   return (
     <>
        <div className={containerStyle}>
