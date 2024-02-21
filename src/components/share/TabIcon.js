96af7cce5f514390cf77ae5917c07accbadc4b63
Dylan <dylan.gavilan32@gmail.com>
2024-02-21 17:01:14 -0300

    fix icon

--- src/components/share/TabIcon.js
+++ src/components/share/TabIcon.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { ReactComponent as IconTwitter } from 'assets/images/icon-x.svg'
 import { ReactComponent as IconTwitterAlt } from 'assets/images/icon-x-alt.svg'
-import { ReactComponent as IconSend } from 'assets/images/icon-send.svg'
 import { ReactComponent as IconSendAlt } from 'assets/images/icon-send-alt.svg'
-import { LinkIcon } from "@heroicons/react/24/solid";
+import { ReactComponent as IconLink } from 'assets/images/icon-link.svg'
+
+// import { LinkIcon } from "@heroicons/react/24/solid";
 
 const TabIcon = ({ type }) => {
   const tabIcons = {
     twitter: IconTwitterAlt,
     send: IconSendAlt,
-    link: IconSendAlt,
+    link: IconLink,
   }
   const IconComponent = tabIcons[type];
   return (
