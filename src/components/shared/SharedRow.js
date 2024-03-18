ea9da37465f6b9e7e206c7bc7e2767a029438ebf
Dylan <dylan.gavilan32@gmail.com>
2024-03-18 16:21:33 -0300

    cweb 81

--- src/components/shared/SharedRow.js
+++ src/components/shared/SharedRow.js
@@ -46,11 +46,14 @@ const SharedRow = ({ track, isMobile }) => {
 
   return (
     <>
-      <tr className={`hover:!rounded-xl hover:bg-neutral-silver-700 text-left ${track.plays === track.play_limit && '!opacity-50'}`}>
-        <td className='md:w-[35%] mr-3'>
+      <tr onClick={handleOnCoverClick} style={{height: '5rem'}} 
+          onMouseEnter={() => setHover(true)} 
+          onMouseLeave={() => setHover(false)} 
+          className='hover:bg-neutral-silver-600 text-left'>
+        <td className='md:!pl-5'>
           <div className='flex flex-row gap-3 md:gap-4'>
             <div
-              className='w-11 h-11 bg-cover rounded'
+              className='w-[52px] h-[52px] min-w-[52px] bg- bg-cover rounded'
               style={{ backgroundImage: `url(${track.cover_url})` }}
               onClick={handleOnCoverClick}
               onMouseEnter={toggleHover}
@@ -59,7 +62,7 @@ const SharedRow = ({ track, isMobile }) => {
               {play && playlist[0]?.isPlaying && <div className='cover-hover'><PauseIcon className='h-6 w-6 text-white' /></div>}
             </div>
             <div>
-              <div className='text-base md:text-lg line-clamp-1' >{track.title}</div>
+              <div className='text-base md:text-lg text-left line-clamp-1' >{track.title}</div>
               <div className='text-sm text-neutral-silver-200'>
                 {isMobile ? `${track.version + ' - ' + track.plays}` : track.authors.join(', ')}
               </div>
@@ -68,15 +71,15 @@ const SharedRow = ({ track, isMobile }) => {
         </td>
         {!isMobile &&
           <>
-            <td className='md:w-[20%] md:mr-3 hover:underline'>{track.album ? track.album : 'any'}</td>
-            <td className='md:w-[15%] md:mr-3'>{track.version}</td>
-            <td className='md:w-[15%] md:mr-3'>
+            <td className='hover:underline'>{track.album ? track.album : 'any'}</td>
+            <td>{track.version}</td>
+            <td>
               <span className='capitalize text-base'>
                 {timeDifference(track.date_shared ? track.date_shared : track.date_played)}
               </span>
             </td>
-            <td className='w-[10%] md:!mr-3'>{format.time(track.length)}</td>
-            <td className='w-[10%] md:!mr-3'>{track.play_limit ? `${track.plays} / ${track.play_limit}` : track?.plays}</td>
+            <td>{format.time(track.length)}</td>
+            <td>{track.play_limit ? `${track.plays} / ${track.play_limit}` : track?.plays}</td>
           </>
         }
       </tr>
