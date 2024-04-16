c68fea25b514bad071b9e3dfd031c8cb53098e46
Dylan <dylan.gavilan32@gmail.com>
2024-04-16 11:46:23 -0300

    fix 16

--- src/components/toasts/Toast.js
+++ src/components/toasts/Toast.js
@@ -7,19 +7,26 @@ import { XMarkIcon } from '@heroicons/react/24/outline';
 const Toast = ({ type, title, body, close }) => {
   const icons = {
     copy: <DocumentDuplicateIcon className='text-brand-gold h-7 w-7'/>
+  };
+
+  const icon = icons[type];
+
+  const cutName = (name) => {
+    return name.length > 15 ? name.slice(0, 12) + '...' : name;
   }
-  const icon = icons[type]
   return (
     <>
-        <div className='w-[22rem]  flex gap-4 !bg-neutral-silver-600 p-3 !rounded-2xl'>
-            <div className='p-3 rounded-xl bg-neutral-black'>
-                {icon}
+        <div className='w-[340px] lg:w-[430px]  flex justify-between gap-4 !bg-neutral-silver-600 p-3 !rounded-2xl'>
+            <div className='flex gap-4'>
+              <div className='p-3 rounded-xl bg-neutral-black'>
+                  {icon}
+              </div>
+              <div className='!text-left'>
+                  <p className='lg:!text-lg !text-base font-semibold'>{title}</p>
+                  <p className='!text-base !text-left !text-neutral-silver-200'>{cutName(body)}</p>
+              </div>
             </div>
-            <div className='!text-left'>
-                <p className='!text-lg font-semibold'>{title}</p>
-                <p className='!text-base !text-left !text-neutral-silver-200'>{body}</p>
-            </div>
-            <button className='p-2 flex items-center' onClick={close}>
+            <button className='p-2 items-center lg:block hidden' onClick={close}>
                 <XMarkIcon className='!text-neutral-silver-200 h-6 w-6'/>
             </button>
         </div>
