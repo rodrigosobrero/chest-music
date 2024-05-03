3a6a03e612d16acdaac8a57b298c227eed402cfe
Rodrigo Sobrero <hola@rodrigosobrero.com>
2024-05-02 22:21:36 -0300

    fix CWEB-44

--- src/routes/password-reset.js
+++ src/routes/password-reset.js
@@ -1,15 +1,51 @@
 import Input from 'components/Input';
 import Button from 'components/Button';
+import { useTranslation } from 'react-i18next';
+import { auth } from 'utils/firebase';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 export default function PasswordReset() {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const handleReset = (data) => {
+    sendPasswordResetEmail(auth, data.email)
+      .then(() => {
+        console.log('test')
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   return (
     <>
       <div className='flex flex-col items-center justify-center w-full h-full'>
         <h1 className=' text-[76px]'>password reset</h1>
-        <p className='text-neutral-silver-200'>Enter your email address here to continue with your password reset</p>
-        <div className=' w-72'>
-          <Input type='email' label='Email' placeholder='Write here...' />
-          <Button type='button' style=''></Button>
+        <p className='text-neutral-silver-200 mb-6'>Enter your email address here to continue with your<br /> password reset</p>
+        <div className='max-w-[480px] w-full'>
+          <form onSubmit={handleSubmit(handleReset)} className='w-full'>
+            <div className='flex flex-col gap-6 w-full'>
+              <Input
+                type='email'
+                label='Email'
+                name='email'
+                register={register}
+                required={true}
+                error={errors.email && t('global.required')}
+                placeholder={t('global.write_here')} />
+              <Button type='submit' style='primary' text='Send reset' disabled={errors.email} />
+              <Button type='button' customStyle='text-brand-gold font-bold' text='Cancel' onClick={() => { navigate('/sign-in') }} />
+            </div>
+          </form>
         </div>
       </div>
     </>
