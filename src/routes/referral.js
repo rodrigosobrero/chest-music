46064bb76902f02e51c18e7e15f5fdba6dc1d068
Mateo Reynoso <mateoreynoso2009@gmail.com>
2024-06-18 11:21:05 -0300

    copy to clipboard referral code button

--- src/routes/referral.js
+++ src/routes/referral.js
@@ -15,8 +15,16 @@ const Referral = () => {
   let paths = [{ name: t('global.profile'), link: '/profile' }, { name: items[0].title }]
   
   const user = useSelector((state) => state.auth.user)
-  const { data, handleToggle , isFetching } = useFetch(process.env.REACT_APP_API + 'globalpermission/', user?.token )
+  const { data, handleToggle , isFetching } = useFetch(process.env.REACT_APP_API + 'account/referrals/', user?.token )
   
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(data?.ambassador_url).then(() => {
+      console.log(data?.ambassador_url);
+      console.log(data);
+    }).catch((err) => {
+      console.error('Failed to copy text: ', err);
+    });
+  }
 
 
   return (
@@ -38,14 +46,14 @@ const Referral = () => {
             <h4 className='!text-lg !font-archivo !normal-case w-4/5'>{t('referral.share_code')}</h4>
             <button
              className='flex items-center group'
-             onClick={()=>console.log(user?.token)}
+             onClick={copyToClipboard}
              >
                 <h4 className='!text-lg !font-archivo !normal-case mr-1 text-brand-gold group-hover:text-orange-400'>{t('referral.copy')}</h4>
                 <Square2StackIcon className='inline-flex h-6 w-6 text-brand-gold group-hover:text-orange-400' />
             </button>
             </div>
             <div className='bg-neutral-silver-700 rounded-3xl md:p-8 py-8 px-6 flex flex-row'>
-            <h4 className='!text-lg !font-archivo !normal-case text-neutral-silver-300 truncate'>https://chestmusic.com/beta/1294tn2389sfgeh</h4>
+            <h4 className='!text-lg !font-archivo !normal-case text-neutral-silver-300 truncate'>{data?.ambassador_url}</h4>
             </div>
             </div>
 
